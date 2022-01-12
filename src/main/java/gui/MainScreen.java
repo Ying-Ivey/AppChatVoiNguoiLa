d7f8604d42fbc76a31083d8ed95d44c61c4051d9
thuydoanh <truongthuydoanh99@gmail.com>
2022-01-12 08:00:20 +0700

    [MODIFIED]: Update MainScreen

--- src/main/java/gui/MainScreen.java
+++ src/main/java/gui/MainScreen.java
@@ -24,41 +24,41 @@ import socket_connection_client.DataSocket;
 import socket_connection_client.SocketConnection;
 import socket_connection_client.SocketHandler;
 
-public class MainScreen extends JFrame{
+public class MainScreen extends JFrame {
 	private Container c;
 	private JLabel title, title2;
 	private JButton btnPairing, btnExit;
 	SocketConnection socket = new SocketConnection();
-    DataSocket dataSocket = new DataSocket();
+	DataSocket dataSocket = new DataSocket();
+
 	public MainScreen() {
 		init();
-		
+
 		socket.addListenConnection("return_login", new SocketHandler() {
-            @Override
-            public void onHandle(JSONObject data, BufferedReader in, BufferedWriter out) {
+			@Override
+			public void onHandle(JSONObject data, BufferedReader in, BufferedWriter out) {
 				try {
 					Login login = new Login();
 					login.setVisible(true);
-	                dispose();
+					dispose();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-            }
-        });
-		
+			}
+		});
+
 		addWindowListener(new WindowListener() {
 
 			@Override
 			public void windowClosing(WindowEvent e) {
 				String ObjButtons[] = { "Có", "Không" };
-				int PromptResult = JOptionPane.showOptionDialog(null, "Bạn có muốn rời khỏi đây không?",
-						"Đăng xuất", JOptionPane.PLAIN_MESSAGE, JOptionPane.WARNING_MESSAGE, null, ObjButtons,
-						ObjButtons[0]);
+				int PromptResult = JOptionPane.showOptionDialog(null, "Bạn có muốn rời khỏi đây không?", "Đăng xuất",
+						JOptionPane.PLAIN_MESSAGE, JOptionPane.WARNING_MESSAGE, null, ObjButtons, ObjButtons[0]);
 				if (PromptResult == JOptionPane.YES_OPTION) {
 					String dataSend = dataSocket.exportData_ReturnLogin(User.nickname);
-			        socket.sendData(dataSend);
-			        dispose();
-			        System.out.println("User rời khỏi màn hình chính");
+					socket.sendData(dataSend);
+					dispose();
+					System.out.println("User rời khỏi màn hình chính");
 				}
 			}
 
@@ -99,10 +99,9 @@ public class MainScreen extends JFrame{
 			}
 
 		});
-		
-		
+
 	}
-	
+
 	private void init() {
 		setTitle("ỨNG DỤNG CHAT VỚI NGƯỜI LẠ");
 		setBounds(350, 150, 540, 390);
@@ -110,19 +109,19 @@ public class MainScreen extends JFrame{
 		setResizable(false);
 		c = getContentPane();
 		c.setLayout(null);
-		
+
 		title = new JLabel("ỨNG DỤNG CHAT VỚI NGƯỜI LẠ");
 		title.setFont(new Font("Arial", Font.PLAIN, 20));
 		title.setSize(350, 30);
 		title.setLocation(90, 50);
 		c.add(title);
-		
+
 		title2 = new JLabel("Chúc bạn có những giây phút vui vẻ ^^!");
 		title2.setFont(new Font("Arial", Font.PLAIN, 20));
 		title2.setSize(350, 30);
 		title2.setLocation(75, 150);
 		c.add(title2);
-		
+
 		btnPairing = new JButton("GHÉP ĐÔI");
 		btnPairing.setFont(new Font("Arial", Font.PLAIN, 15));
 		btnPairing.setSize(120, 20);
@@ -130,7 +129,7 @@ public class MainScreen extends JFrame{
 		btnPairing.setForeground(Color.decode("#249ce3"));
 		btnPairing.setFocusPainted(false);
 		c.add(btnPairing);
-		
+
 		btnExit = new JButton("THOÁT");
 		btnExit.setFont(new Font("Arial", Font.PLAIN, 15));
 		btnExit.setSize(100, 20);
@@ -138,22 +137,22 @@ public class MainScreen extends JFrame{
 		btnExit.setForeground(Color.decode("#de1912"));
 		btnExit.setFocusPainted(false);
 		c.add(btnExit);
-		
+
 		setVisible(true);
 		addEventListeners();
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-        
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-            	new MainScreen().setVisible(true);
-            }
-        });
-    }
+
+		java.awt.EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				new MainScreen().setVisible(true);
+			}
+		});
+	}
 
 	public void addEventListeners() {
-		//System.exit(0);
+		// System.exit(0);
 		btnPairing.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				WaitingForPairing waitingForPairing = new WaitingForPairing();
@@ -162,12 +161,18 @@ public class MainScreen extends JFrame{
 			}
 
 		});
-		
+
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String dataSend = dataSocket.exportData_ReturnLogin(User.nickname);
-		        socket.sendData(dataSend);
-		        dispose();
+				String ObjButtons[] = { "Có", "Không" };
+				int PromptResult = JOptionPane.showOptionDialog(null, "Bạn có muốn rời khỏi đây không?", "Đăng xuất",
+						JOptionPane.PLAIN_MESSAGE, JOptionPane.WARNING_MESSAGE, null, ObjButtons, ObjButtons[0]);
+				if (PromptResult == JOptionPane.YES_OPTION) {
+					String dataSend = dataSocket.exportData_ReturnLogin(User.nickname);
+					socket.sendData(dataSend);
+					dispose();
+					System.out.println("User rời khỏi màn hình chính");
+				}
 			}
 		});
 	}
